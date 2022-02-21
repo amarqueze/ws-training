ec1e37b6129a4854341da7b9416cadbebd171e5d
amarqueze <alanmarquez@outlook.com>
2022-02-21 21:39:41 +0000

    access login

--- IncomesvExpenses/src/app/pages/login/login.component.ts
+++ IncomesvExpenses/src/app/pages/login/login.component.ts
@@ -1,25 +1,35 @@
 import { Session } from './../../share/domain/Session.interface';
 import { selectAuthSessionExist } from './../../share/store/auth/auth.selector';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { signUp } from 'src/app/share/store/auth/auth.actions';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
+  private _subscriptions = new Subscription();
   session$ = this.store.pipe(selectAuthSessionExist);
 
-  constructor(private store: Store<any>) { }
+  constructor(private store: Store<any>, private router: Router) {}
 
   ngOnInit(): void {
-    this.session$.subscribe(sessionCurrent => this.confirmSession(sessionCurrent));
+    this._subscriptions.add(
+      this.session$.subscribe(sessionCurrent => this.confirmSession(sessionCurrent)));
+      
     this.store.dispatch(signUp({nameFull: "Berton Suarez", email: "bertonz@outlook.com", password: "123456"}));
+  }
+
+  ngOnDestroy(): void {
+    this._subscriptions.unsubscribe();
   }
 
   confirmSession(session: Session) {
     console.log(session);
+    this.router.navigate(["/dashboard"]);
   }
 }
