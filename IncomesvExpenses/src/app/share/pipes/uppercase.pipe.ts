74f00e33e0ce6cb22e6cfa4c2d0fd60cab3741af
amarqueze <alanmarquez@outlook.com>
2022-04-15 23:35:13 +0000

    added pipes angular

---
+++ IncomesvExpenses/src/app/share/pipes/uppercase.pipe.ts
@@ -0,0 +1,16 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'uppercase',
+})
+export class UppercasePipe implements PipeTransform {
+
+  transform(value: string): string {
+    if(!value) {
+      return '';
+    }
+
+    return value.toUpperCase();
+  }
+
+}
--- IncomesvExpenses/src/app/share/share.module.ts
+++ IncomesvExpenses/src/app/share/share.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TtClassDirectiveDirective } from './directives/tt-class-directive.directive';
+import { UppercasePipe } from './pipes/uppercase.pipe';
 
 @NgModule({
   declarations: [
-    TtClassDirectiveDirective
+    TtClassDirectiveDirective,
+    UppercasePipe
   ],
   exports: [
-    TtClassDirectiveDirective
+    TtClassDirectiveDirective,
+    UppercasePipe
   ],
   imports: [
     CommonModule
